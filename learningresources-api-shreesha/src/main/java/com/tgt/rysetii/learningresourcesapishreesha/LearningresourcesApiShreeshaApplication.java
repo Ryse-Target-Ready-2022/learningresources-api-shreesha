addb9a9b1f71cbf2cf2e4950efff96d7ff613401
Shreesham43 <shreesham43@gmail.com>
2022-07-02 16:10:05 +0530

    Hello World Added

---
+++ learningresources-api-shreesha/src/main/java/com/tgt/rysetii/learningresourcesapishreesha/LearningresourcesApiShreeshaApplication.java
@@ -0,0 +1,14 @@
+package com.tgt.rysetii.learningresourcesapishreesha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningresourcesApiShreeshaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningresourcesApiShreeshaApplication.class, args);
+		System.out.println("Hello World!!");
+	}
+
+}
---
+++ learningresources-api-shreesha/src/test/java/com/tgt/rysetii/learningresourcesapishreesha/LearningresourcesApiShreeshaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgt.rysetii.learningresourcesapishreesha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningresourcesApiShreeshaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
