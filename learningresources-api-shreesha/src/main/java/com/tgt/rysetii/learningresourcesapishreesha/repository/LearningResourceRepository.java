cce0d47e6418267f74ba517139f924de05413421
Shreesham43 <shreesham43@gmail.com>
2022-07-12 14:25:44 +0530

    Added JPA with H2 services

--- learningresources-api-shreesha/src/main/java/com/tgt/rysetii/learningresourcesapishreesha/repository/LearningResourceRepository.java
+++ learningresources-api-shreesha/src/main/java/com/tgt/rysetii/learningresourcesapishreesha/repository/LearningResourceRepository.java
@@ -1,2 +1,9 @@
-package com.tgt.rysetii.learningresourcesapishreesha.repository;public interface LearningResourceRepository {
+package com.tgt.rysetii.learningresourcesapishreesha.repository;
+
+import com.tgt.rysetii.learningresourcesapishreesha.entity.LearningResource;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LearningResourceRepository extends JpaRepository<LearningResource, Integer> {
 }
--- learningresources-api-shreesha/src/main/java/com/tgt/rysetii/learningresourcesapishreesha/service/LearningResourceService.java
+++ learningresources-api-shreesha/src/main/java/com/tgt/rysetii/learningresourcesapishreesha/service/LearningResourceService.java
@@ -2,6 +2,8 @@ package com.tgt.rysetii.learningresourcesapishreesha.service;
 
 import com.tgt.rysetii.learningresourcesapishreesha.entity.LearningResource;
 import com.tgt.rysetii.learningresourcesapishreesha.entity.LearningResourceStatus;
+import com.tgt.rysetii.learningresourcesapishreesha.repository.LearningResourceRepository;
+import org.springframework.stereotype.Service;
 
 import java.io.*;
 import java.time.LocalDate;
@@ -12,86 +14,21 @@ import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 
+@Service
 public class LearningResourceService {
+    private final LearningResourceRepository learningResourceRepository;
+
+    public LearningResourceService(LearningResourceRepository learningResourceRepository) {
+        this.learningResourceRepository = learningResourceRepository;
+    }
+
     private List<LearningResource> getLearningResources(){
-        File learningResourceFile = new File("LearningResource.csv");
-        List<LearningResource> learningResources = loadLearningResourcesFromCSV(learningResourceFile);
-        return learningResources;
-    }
-
-    private List<LearningResource> loadLearningResourcesFromCSV(File learningResourceFile) {
-        List<LearningResource> learningResources = new ArrayList<>();
-        try{
-            FileReader fileReader = new FileReader(learningResourceFile);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-
-            String line = bufferedReader.readLine();
-            while (line != null){
-                String[] attributes = line.split(",");
-                LearningResource learningResource = createLearningResource(attributes);
-                learningResources.add(learningResource);
-                line = bufferedReader.readLine();
-            }
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-
-        return learningResources;
-    }
-
-    private LearningResource createLearningResource(String[] attributes) {
-        Integer learningResourceId = Integer.parseInt(attributes[0].replaceAll("\\D", ""));
-        String learningResourceName = attributes[1];
-        Double costPrice = Double.parseDouble(attributes[2]);
-        Double sellingPrice = Double.parseDouble(attributes[3]);
-        LearningResourceStatus learningResourceStatus = LearningResourceStatus.valueOf(attributes[4]);
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        LocalDate createdDate = LocalDate.parse(attributes[5], dateTimeFormatter);
-        LocalDate publishedDate = LocalDate.parse(attributes[6], dateTimeFormatter);
-        LocalDate retiredDate = LocalDate.parse(attributes[7], dateTimeFormatter);
-
-        LearningResource learningResource = new LearningResource(
-                learningResourceId, learningResourceName, costPrice, sellingPrice, learningResourceStatus,createdDate,publishedDate,retiredDate
-        );
-
-        return learningResource;
+        return learningResourceRepository.findAll();
     }
 
     public void saveLearningResources(List<LearningResource> learningResources){
-        populateLearningResourcesToCSV(learningResources);
-    }
-
-    private void populateLearningResourcesToCSV(List<LearningResource> learningResources) {
-        final String csvDelimiter = ",";
-        try{
-            File learningResourcesFile = new File("LearningResources.csv");
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(learningResourcesFile.getName(), true) );
-            for (LearningResource learningResource:learningResources) {
-                bufferedWriter.newLine();
-                StringBuffer singleLine = new StringBuffer();
-                singleLine.append(learningResource.getLearningResourceId());
-                singleLine.append(csvDelimiter);
-                singleLine.append(learningResource.getLearningResourceName());
-                singleLine.append(csvDelimiter);
-                singleLine.append(learningResource.getCostPrice());
-                singleLine.append(csvDelimiter);
-                singleLine.append(learningResource.getSellingPrice());
-                singleLine.append(csvDelimiter);
-                singleLine.append(learningResource.getLearningResourceStatus());
-                singleLine.append(csvDelimiter);
-                singleLine.append(learningResource.getCreatedDate());
-                singleLine.append(csvDelimiter);
-                singleLine.append(learningResource.getPublishedDate());
-                singleLine.append(csvDelimiter);
-                singleLine.append(learningResource.getRetiredDate());
-                singleLine.append(csvDelimiter);
-                bufferedWriter.write(learningResource.toString());
-            }
-            bufferedWriter.flush();
-            bufferedWriter.close();
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+        for (LearningResource learningResource: learningResources)
+            learningResourceRepository.save(learningResource);
     }
 
     public List<Double> getProfitMargin(){
